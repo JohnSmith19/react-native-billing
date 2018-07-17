2a795d0b3f3d0f7dd18bdc2ad433388ae4610473
John Smith <unknown.prop@gmail.com>
2018-07-17 09:40:56 +0900

    정적응답 테스트

--- App.js
+++ App.js
@@ -4,35 +4,129 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   Platform,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+  Button,
+  ActivityIndicator,
+  TextInput
+} from "react-native";
+
+import InAppBilling from "react-native-billing";
+
+const defaultState = {
+  productDetails: null,
+  transactionDetails: null,
+  consumed: false,
+  error: null
+};
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  state = {
+    productId: "android.test.purchased",
+    ...defaultState
+  };
+
+  resetState = () => {
+    this.setState(defaultState);
+  };
+
+  getProductDetails = async () => {
+    console.log("getProductDetails");
+    try {
+      this.resetState();
+      await InAppBilling.open();
+      const details = await InAppBilling.getProductDetails(
+        this.state.productId
+      );
+      await InAppBilling.close();
+      this.setState({ productDetails: JSON.stringify(details) });
+    } catch (err) {
+      this.setState({ error: JSON.stringify(err) });
+      await InAppBilling.close();
+    }
+  };
+
+  purchaseProduct = async () => {
+    console.log("purchaseProduct");
+    try {
+      this.resetState();
+      await InAppBilling.open();
+      const details = await InAppBilling.purchase(this.state.productId);
+      console.log(details);
+      await InAppBilling.close();
+      this.setState({ transactionDetails: JSON.stringify(details) });
+    } catch (err) {
+      this.setState({ error: JSON.stringify(err) });
+      await InAppBilling.close();
+    }
+  };
+
+  consumePurchase = async () => {
+    console.log("consumePurchase");
+    try {
+      this.resetState();
+      await InAppBilling.open();
+      const details = await InAppBilling.consumePurchase(this.state.productId);
+      console.log(details);
+      await InAppBilling.close();
+      this.setState({ consumed: true });
+    } catch (err) {
+      this.setState({ error: JSON.stringify(err) });
+      await InAppBilling.close();
+    }
+  };
+
+  updateProductId = productId => {
+    console.log("updateProductId: " + productId);
+    this.setState({ productId });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Text style={styles.welcome}>React Native Billing</Text>
+        <Text style={styles.instructions}>To get started, edit product Id</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+
+        <TextInput
+          onChangeText={this.updateProductId}
+          value={this.state.productId}
+        />
+        <Button onPress={this.getProductDetails} title="Get product details" />
+        {this.state.productDetails && (
+          <Text style={styles.text}>{this.state.productDetails}</Text>
+        )}
+        <Button
+          onPress={this.purchaseProduct}
+          title={"Purchase " + this.state.productId}
+        />
+        {this.state.transactionDetails && (
+          <Text style={styles.text}>{this.state.transactionDetails}</Text>
+        )}
+        <Button
+          onPress={this.consumePurchase}
+          title={"Consume " + this.state.productId}
+        />
+        {this.state.consumed && (
+          <Text style={styles.text}>Purchase consumed</Text>
+        )}
+        {this.state.error && (
+          <Text style={[styles.text, { color: "red", marginTop: 10 }]}>
+            Error:{"\n"}
+            {this.state.error}
+          </Text>
+        )}
       </View>
     );
   }
@@ -41,18 +135,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # React Native Billing
 
-이 문서는 react-native-billing 모듈을 이용하여 Android 플랫폼의 In App Bliing 을 구현하는 방법을 설명합니다.
+이 문서는 [react-native-billing](https://github.com/idehub/react-native-billing) 모듈을 이용하여 Android 플랫폼의 In App Bliing 을 구현하는 방법을 설명합니다.
 
 ## 프로젝트 생성
 
-윈도우 환경에서 아래의 명령을 입력하여 프로젝트를 생성하고 구동합니다.
+윈도우 환경에서는 아래의 명령을 입력하여 프로젝트를 생성하고 구동합니다.
 
 ```bash
 react-native init --version="0.55.4" rnb
@@ -71,4 +71,44 @@ Google Play license key 를 아래의 리소스 항목에 입력합니다.
 
 ```java
 .addPackage(new InAppBillingBridgePackage("YOUR_LICENSE_KEY"))
+```
+
+## 테스트
+
+### [정적 응답 테스트](http://developer.android.com/google/play/billing/billing_testing.html#billing-testing-static)
+
+구글에서 미리 정의한 상품 아이디로 정적 응답 테스트를 진행할 수 있습니다.
+
+- android.test.purchased
+- android.test.canceled
+- android.test.refunded
+- android.test.item_unavailable
+
+정적 테스트를 진행하기 위해서는 license key 를 null 로 설정 합니다.
+
+[See the Google Play docs for more info on static responses.](https://developer.android.com/google/play/billing/billing_testing#billing-testing-static)
+
+For instance to purchase and consume the static android.test.purchased products, with async/await (you can chain the promise) :
+
+```js
+// To be sure the service is close before opening it
+async pay() {
+  await InAppBilling.close();
+  try {
+    await InAppBilling.open();
+    if (!await InAppBilling.isPurchased(productId)) {
+      const details = await InAppBilling.purchase(productId);
+      console.log('You purchased: ', details);
+    }
+    const transactionStatus = await InAppBilling.getPurchaseTransactionDetails(productId);
+    console.log('Transaction Status', transactionStatus);
+    const productDetails = await InAppBilling.getProductDetails(productId);
+    console.log(productDetails);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await InAppBilling.consumePurchase(productId);
+    await InAppBilling.close();
+  }
+}
 ```
